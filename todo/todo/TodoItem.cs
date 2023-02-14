4fc8adf0d06d82bf895cde5567b4f241d9a1ddfa
Piotr Andrzejewski <xividi123@gmail.com>
2023-02-14 20:51:24 +0100

    idk

---
+++ todo/todo/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿ using System;
+namespace todo
+{
+	public class TodoItem
+	{
+		public string TodoText { get; set; }
+		public bool Complete { get; set; }
+
+		public TodoItem(string TodoText, bool Complete)
+		{
+			this.TodoText = TodoText;
+			this.Complete = Complete;
+		}
+	}
+}
+
---
+++ todo/todo/TodoListViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms; 
+
+namespace todo
+{
+	public class TodoListViewModel
+	{
+		public ObservableCollection<TodoItem> TodoItems { get; set; }
+
+		public TodoListViewModel()
+		{
+			TodoItems = new ObservableCollection<TodoItem>();
+
+			TodoItems.Add(new TodoItem("item 1", true));
+			TodoItems.Add(new TodoItem("item 2", false));
+			TodoItems.Add(new TodoItem("item 3", false));
+
+		}
+
+		//command will execute when 
+		public ICommand AddTodoCommand => new Command(AddTodoItem); 
+		public string TodoInputValue { get; set; }
+		//listening for a change to push to the command
+		void AddTodoItem()
+		{
+			TodoItems.Add(new TodoItem(TodoInputValue, false));
+		}
+
+		public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+		void RemoveTodoItem(object o)
+		{
+			TodoItem todoItemBeingRemoved = o as TodoItem;
+			TodoItems.Remove(todoItemBeingRemoved);
+		}
+
+    }
+}
+
